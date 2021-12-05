46546d8862dd6c36899bc128bf5bdddaaf0e4fb1
Oleh Semennykov <sumcue@gmail.com>
2021-12-05 20:30:21 +0200

    init statick gallery

---
+++ src/js/gallery-items.js
@@ -0,0 +1,161 @@
+export const cards = [
+    {
+        // mobileImage : '../images/Rectangle 5.jpg' ,
+        // doubleMobileImage : '../images/Rectangle 5@2 x2 320.jpg',
+        // tablettImage : '../images/Rectangle 5 768.jpg',
+        // doubleTabletImage : '../images/Rectangle 5@2 x2 768.jpg',    
+        'desctopImage' : '../images/Rectangle 5 1024.jpg',
+        'doubleDesctopImage' : '../images/Rectangle 5@2 1024.jpg',
+        'Original_title' : 'Greyhound',
+
+        'genres' : [
+{
+    'genre': 'Action',
+},
+{
+    'genre' : 'drama'
+}
+
+
+        ],
+            
+               
+            
+        
+    
+    'release_date' : '2020',
+    },
+ {
+    mobileImage : '../images/Rectangle 12.jpg' ,
+    doubleMobileImage : '../images/Rectangle 12@2 x2 320.jpg',
+    tablettImage : '../images/Rectangle 12 768.jpg',
+    doubleTabletImage : '../images/Rectangle 12@2 x2 768.jpg',
+    desctopImage : '../images/Rectangle 12 1024.jpg',
+    doubleDesctopImage : '../images/Rectangle 12@2 x2 1024.jpg',
+    'Original_title' : 'Monster hunter',
+
+    genres : [ {
+        genre : '',
+    }
+],
+
+'release_date' : '',
+ },
+
+ {
+    mobileImage : '../images/Rectangle 13.jpg' ,
+    doubleMobileImage : '../images/Rectangle 13@2 x2 320.jpg',
+    tablettImage : '../images/Rectangle 13 768.jpg',
+    doubleTabletImage : '../images/Rectangle 13@2 x2 768.jpg',
+    desctopImage : '../images/Rectangle 13 1024.jpg',
+    doubleDesctopImage : '../images/Rectangle 13@2 x2 1024.jpg',
+    Original_title : 'Once upon a time... in Hollywod',
+
+    genres : [ {
+        genre : '',
+    }
+],
+
+release_date : '',
+ },
+
+ {
+    mobileImage : '../images/Rectangle 14.jpg' ,
+    doubleMobileImage : '../images/Rectangle 14@2  x2 320.jpg',
+    tablettImage : '../images/Rectangle 14 768.jpg',
+    doubleTabletImage : '../images/Rectangle 14@2 x2 768.jpg',
+    desctopImage : '../images/Rectangle 14 1024.jpg',
+    doubleDesctopImage : '../images/Rectangle 14@2 x2 1024.jpg',
+    Original_title : 'Little women',
+   
+    genres : [ {
+        genre : '',
+    }
+   ],
+   
+   release_date : '',
+ },
+
+ {
+    mobileImage : '../images/Rectangle 15.jpg' ,
+    doubleMobileImage : '../images/Rectangle 15@2 x2 320.jpg',
+    tablettImage : '../images/Rectangle 15 768.jpg',
+    doubleTabletImage : '../images/Rectangle 15@2 x2 768.jpg',
+    desctopImage : '../images/Rectangle 15 1024.jpg',
+    doubleDesctopImage : '../images/Rectangle 15@2 x2 1024.jpg',
+    Original_title : 'Aquarela',
+   
+    genres : [ {
+        genre : '',
+    }
+   ],
+   
+   release_date : '',
+ },
+
+ {
+    mobileImage : '../images/Rectangle 16.jpg' ,
+    doubleMobileImage : '../images/Rectangle 16@2 x2 320.jpg',
+    tablettImage : '../images/Rectangle 16 768.jpg',
+    doubleTabletImage : '../images/Rectangle 16@2 x2 768.jpg',
+    desctopImage : '../images/Rectangle 16 1024.jpg',
+    doubleDesctopImage : '../images/Rectangle 16@2 x2 1024.jpg',
+    Original_title : 'David Crosby',
+   
+    genres : [ {
+        genre : '',
+    }
+   ],
+   
+   release_date : '',
+ },
+
+ {
+    mobileImage : '../images/Rectangle 17.jpg' ,
+    doubleMobileImage : '../images/Rectangle 17@2 x2 320.jpg',
+    tablettImage : '../images/Rectangle 17 768.jpg',
+    doubleTabletImage : '../images/Rectangle 17@2 x2 768.jpg',
+    desctopImage : '../images/Rectangle 17 1024.jpg',
+    doubleDesctopImage : '../images/Rectangle 17@2 x2 1024.jpg',
+    Original_title : 'Moon',
+   
+    genres : [ {
+        genre : '',
+    }
+   ],
+   
+   release_date : '',
+ },
+
+ {
+    mobileImage : '../images/Rectangle 18.jpg' ,
+    doubleMobileImage : '../images/Rectangle 18@2 x2 320.jpg',
+    tablettImage : '../images/Rectangle 18 768.jpg',
+    doubleTabletImage : '../images/Rectangle 18@2 x2 768.jpg',
+    desctopImage : '../images/Rectangle 18 1024.jpg',
+    doubleDesctopImage : '../images/Rectangle 18@2 x2 1024.jpg',
+    Original_title : 'Fistfull of lead',
+   
+    genres : [ {
+        genre : '',
+    }
+   ],
+   
+   release_date : '',
+ },
+
+ {
+   desctopImage : '../images/Rectangle 19 1024.jpg',
+    doubleDesctopImage : '../images/Rectangle 19@2 x2 1024.jpg',
+    Original_title : ' Bad Boys',
+   
+    genres : [ {
+        genre : '',
+    }
+   ],
+   
+   release_date : '',
+ },
+
+
+] 
