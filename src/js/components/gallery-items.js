f0ce24b365a3df9b981a5c2958301e40bf6abee1
Sergey <sergey.nosan@gmail.com>
2021-12-07 01:58:17 +0200

    add edits

--- src/js/components/gallery-items.js
+++ src/js/components/gallery-items.js
@@ -1,174 +1,166 @@
 export const cards = [
-    {
-        'image': "../../images/Rectangle 5 1024.jpg" ,
-        'doubleImage' : "../../images/Rectangle 5@2 1024.jpg 2x",
-      'discription' : 'Greyhound',
-        'Original_title' : 'Greyhound',
+  {
+    image: '../../images/Rectangle 5 1024.jpg',
+    doubleImage: '../../images/Rectangle 5@2 1024.jpg 2x',
+    discription: 'Greyhound',
+    Original_title: 'Greyhound',
 
-        'genres' : [
-{
-    'genre': 'Action',
-},
-{
-    'genre' : 'Drama',
-}
-
-
-        ],
-            
-               
-            
-        
-    
-    'release_date' : '2020',
-    },
- {
-    'image' : '../images/Rectangle 12.jpg' ,
-    'doubleImage' : '../images/Rectangle 12@2 x2 320.jpg',
-    'discription' : 'Monster Hunter',
-    'Original_title' : 'Monster Hunter',
-
-    'genres' : [ {
-         'genre': 'Drama',
-    },
-    {
-        'genre' : 'Action',
-
-        
-    },
-],
-
-'release_date' : '2020',
- },
-
- {
-    'image' : '../images/Rectangle 13.jpg' ,
-    'doubleImage' : '../images/Rectangle 13@2  320.jpg x2',
-    'discription' : 'Once upon a time... in Hollywood',
-    'Original_title' : 'Once upon a time...in Hollywood',
-
-    'genres' : [ {
-        'genre' : 'Drama',
-    },
-    {
-        'genre' : 'Comedy',
-    },
-],
-
-'release_date' : '2019',
- },
-
- {
-    'image': "../../images/Rectangle 14 1024.jpg" ,
-    'doubleImage' : "../../images/Rectangle 14@2 1024.jpg 2x",
-    
-    'Original_title' : 'Little women',
-   
-    'genres' : [ {
-        'genre' : 'Drama',
-    }
-   ],
-   
-   'release_date' : '2019',
- },
-
- {
-    'image': "../../images/Rectangle 15 1024.jpg" ,
-    'doubleImage' : "../../images/Rectangle 15@2 1024.jpg 2x",
-    'Original_title' : 'Aquarela',
-   
-    'genres' : [ {
-        'genre' : 'Documentary',
-    },
-    {
-        'genre' : 'Drama',
-    }
-   ],
-   
-   'release_date' : '2019',
- },
-
- {
-    'image': "../../images/Rectangle 16 1024.jpg" ,
-    'doubleImage' : "../../images/Rectangle 16@2 1024.jpg 2x",
-    'Original_title' : 'David Crosby',
-   
-    'genres' : [ {
-        'genre' : 'Documentary',
-    }, {
-        'genre': 'Drama',
-    }
-   ],
-   
-   'release_date' : '2019',
- },
-
- {
-    'image': "../../images/Rectangle 17 1024.jpg" ,
-    'doubleImage' : "../../images/Rectangle 17@2 1024.jpg 2x",
-    'Original_title' : 'Moon',
-   
-    'genres' : [ 
-        {
-        'genre' : 'Sci-Fi',
-    },{
-        'genre' : 'Action'    
-    },
-   ],
-   
-   'release_date' : '2010',
- },
-
- {
-    'image': "../../images/Rectangle 18 1024.jpg" ,
-    'doubleImage' : "../../images/Rectangle 18@2 1024.jpg 2x",
-    'Original_title' : 'Fistfull of lead',
-   
-    'genres' : [ {
-        'genre' : 'Western',
-    }
-   ],
-   
-   'release_date' : '2019',
- },
-
- {
-    'image': "../../images/Rectangle 19 1024.jpg" ,
-    'doubleImage' : "../../images/Rectangle 19@2 1024.jpg 2x",
-    'Original_title' : ' Bad Boys',
-   
-    'genres' : [ {
-        'genre' : 'Action',
-    }
-   ],
-   
-   'release_date' : '2020',
- },
-
- {
-    'image': "../../images/Rectangle 5 1024.jpg" ,
-    'doubleImage' : "../../images/Rectangle 5@2 1024.jpg 2x",
-  'discription' : 'Greyhound',
-    'Original_title' : 'Greyhound',
-
-    'genres' : [
-{
-'genre': 'Action',
-},
-{
-'genre' : 'Drama',
-}
-
-
+    genres: [
+      {
+        genre: 'Action',
+      },
+      {
+        genre: 'Drama',
+      },
     ],
-        
-           
-        
-    
 
-'release_date' : '2020',
-},
+    release_date: '2020',
+  },
+  {
+    image: '../images/Rectangle 12.jpg',
+    doubleImage: '../images/Rectangle 12@2 x2 320.jpg',
+    discription: 'Monster Hunter',
+    Original_title: 'Monster Hunter',
 
+    genres: [
+      {
+        genre: 'Drama',
+      },
+      {
+        genre: 'Action',
+      },
+    ],
 
-] 
+    release_date: '2020',
+  },
 
+  {
+    image: '../images/Rectangle 13.jpg',
+    doubleImage: '../images/Rectangle 13@2  320.jpg x2',
+    discription: 'Once upon a time... in Hollywood',
+    Original_title: 'Once upon a time...in Hollywood',
+
+    genres: [
+      {
+        genre: 'Drama',
+      },
+      {
+        genre: 'Comedy',
+      },
+    ],
+
+    release_date: '2019',
+  },
+
+  {
+    image: '../../images/Rectangle 14 1024.jpg',
+    doubleImage: '../../images/Rectangle 14@2 1024.jpg 2x',
+
+    Original_title: 'Little women',
+
+    genres: [
+      {
+        genre: 'Drama',
+      },
+    ],
+
+    release_date: '2019',
+  },
+
+  {
+    image: '../../images/Rectangle 15 1024.jpg',
+    doubleImage: '../../images/Rectangle 15@2 1024.jpg 2x',
+    Original_title: 'Aquarela',
+
+    genres: [
+      {
+        genre: 'Documentary',
+      },
+      {
+        genre: 'Drama',
+      },
+    ],
+
+    release_date: '2019',
+  },
+
+  {
+    image: '../../images/Rectangle 16 1024.jpg',
+    doubleImage: '../../images/Rectangle 16@2 1024.jpg 2x',
+    Original_title: 'David Crosby',
+
+    genres: [
+      {
+        genre: 'Documentary',
+      },
+      {
+        genre: 'Drama',
+      },
+    ],
+
+    release_date: '2019',
+  },
+
+  {
+    image: '../../images/Rectangle 17 1024.jpg',
+    doubleImage: '../../images/Rectangle 17@2 1024.jpg 2x',
+    Original_title: 'Moon',
+
+    genres: [
+      {
+        genre: 'Sci-Fi',
+      },
+      {
+        genre: 'Action',
+      },
+    ],
+
+    release_date: '2010',
+  },
+
+  {
+    image: '../../images/Rectangle 18 1024.jpg',
+    doubleImage: '../../images/Rectangle 18@2 1024.jpg 2x',
+    Original_title: 'Fistfull of lead',
+
+    genres: [
+      {
+        genre: 'Western',
+      },
+    ],
+
+    release_date: '2019',
+  },
+
+  {
+    image: '../../images/Rectangle 19 1024.jpg',
+    doubleImage: '../../images/Rectangle 19@2 1024.jpg 2x',
+    Original_title: ' Bad Boys',
+
+    genres: [
+      {
+        genre: 'Action',
+      },
+    ],
+
+    release_date: '2020',
+  },
+
+  {
+    image: '../../images/Rectangle 5 1024.jpg',
+    doubleImage: '../../images/Rectangle 5@2 1024.jpg 2x',
+    discription: 'Greyhound',
+    Original_title: 'Greyhound',
+
+    genres: [
+      {
+        genre: 'Action',
+      },
+      {
+        genre: 'Drama',
+      },
+    ],
+
+    release_date: '2020',
+  },
+];
