ebd0e7c9b12996b2b25fc68cea9bc49f4a36d4a5
Sergey <sergey.nosan@gmail.com>
2021-12-08 19:17:17 +0200

    Update gallery.js

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -1,20 +1,10 @@
-import axios from 'axios';
-
 import cardTemplate from '../../template/film-card.hbs';
 import { cards } from './gallery-items';
 
-// const gallery = document.querySelector('.gallery');
-// console.log('gallery', gallery);
-// // console.log(gallery);
-// const markup = cardTemplate(cards);
-// // console.log(cardTemplate(cards))
-// gallery.insertAdjacentHTML('beforeend', markup);
-
-// const fetchMovie = () => {
-//   const movies = axios.get(
-//     'https://api.themoviedb.org/3/movie/500?api_key=e63db2291c5b84e5166b5dd810de73f5',
-//   );
-//   return movies;
-// };
-
-// fetchMovie().then(data => console.log(data.data));
+const gallery = document.querySelector('.gallery');
+console.log('gallery', gallery);
+// console.log(gallery);
+const markup = cardTemplate(cards);
+console.log('markup', markup);
+// console.log(cardTemplate(cards))
+gallery.insertAdjacentHTML('beforeend', markup);
