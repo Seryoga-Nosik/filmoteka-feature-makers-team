9167997e0fe97bd581a28d3317a99994af5c34d4
Sergey <sergey.nosan@gmail.com>
2021-12-09 20:26:03 +0200

    add apiService + change gh-pages

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,10 +1,41 @@
 import axios from 'axios';
 import { BASE_URL, API_KEY } from './constants';
+import noposter from '../images/no-poster.png';
 
 axios.defaults.baseURL = BASE_URL;
 
 export async function fetchMovies() {
-  const movies = await axios.get(`/3/movie/500?api_key=${API_KEY}`);
-  console.log('movies', movies);
-  return movies;
+  try {
+    const response = await axios.get(`/trending/movie/week?api_key=${API_KEY}`);
+    const trendinMoviesData = await response.data;
+    // console.log('trendinMoviesData', trendinMoviesData);
+    const trendinMovies = await trendinMoviesData.results;
+    // console.log('trendinMovies', trendinMovies);
+    return trendinMovies;
+    // return response;
+  } catch (error) {
+    console.error(error);
+  }
 }
+// fetchMovies().then(data => console.log(data));
+
+export async function getGenres() {
+  try {
+    const genres = await axios.get(`/genre/movie/list?api_key=${API_KEY}&language=en-US`);
+    return genres;
+  } catch (error) {
+    console.error(error);
+  }
+}
+// getGenres().then(data => console.log(data));
+
+export async function getMovieInfo(movie_id) {
+  try {
+    const movie = await axios.get(`/movie/${movie_id}?api_key=${API_KEY}&language=en-US`);
+    console.log('movie', movie);
+    return genres;
+  } catch (error) {
+    console.error(error);
+  }
+}
+getMovieInfo().then(data => console.log(data));
--- src/js/constants.js
+++ src/js/constants.js
@@ -1,2 +1,2 @@
 const API_KEY = '9d3b2d59c7632f197e52385ff97229b9';
-const BASE_URL = 'https://api.themoviedb.org';
+const BASE_URL = 'https://api.themoviedb.org/3';
