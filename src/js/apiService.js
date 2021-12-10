d554f6dd57b898ea408614d509751205b8cac2fa
Sergey <sergey.nosan@gmail.com>
2021-12-10 17:20:22 +0200

    add to top btn, add search query fetch

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -3,7 +3,10 @@ import { BASE_URL, API_KEY } from './constants';
 
 axios.defaults.baseURL = BASE_URL;
 
-export async function fetchTrendingMovies() {
+export const genres = {};
+export let totalPages = 1;
+
+export async function getTrendingMovies() {
   try {
     const response = await axios.get(`/trending/movie/week?api_key=${API_KEY}`);
     const trendinMoviesData = await response.data;
@@ -13,24 +16,43 @@ export async function fetchTrendingMovies() {
     console.error(error);
   }
 }
-fetchTrendingMovies().then(data => console.log(data));
+// getTrendingMovies().then(data => console.log(data));
 
 export async function getGenres() {
   try {
-    const genres = await axios.get(`/genre/movie/list?api_key=${API_KEY}&language=en-US`);
+    const response = await axios.get(`/genre/movie/list?api_key=${API_KEY}&language=en-US`);
+    response.data.genres.forEach(({ id, name }) => {
+      genres[id] = name;
+    });
     return genres;
   } catch (error) {
     console.error(error);
   }
 }
-getGenres().then(data => console.log(data));
+// getGenres().then(data => console.log(data));
 
-export async function getMovieInfo(movie_id) {
+export async function getMoviesSearchQuery(searchQuery, page) {
   try {
-    const movie = await axios.get(`/movie/${movie_id}?api_key=${API_KEY}&language=en-US`);
-    return movie;
+    const response = await axios.get(
+      `/search/movie?api_key=${API_KEY}&page=${page}&language=en&query=${searchQuery}`,
+    );
+    const popularMoviesData = await response.data;
+    const popularMovies = await popularMoviesData.results;
+    totalPages = popularMoviesData.total_pages;
+    return popularMovies;
   } catch (error) {
     console.error(error);
   }
 }
-getMovieInfo(512195).then(data => console.log(data));
+// getMoviesSearchQuery().then(data => console.log(data));
+
+export async function getMovieInfo(movie_id) {
+  try {
+    const response = await axios.get(`/movie/${movie_id}?api_key=${API_KEY}&language=en-US`);
+    const movieInfo = await response.data;
+    return movieInfo;
+  } catch (error) {
+    console.error(error);
+  }
+}
+// getMovieInfo(512195).then(data => console.log(data));
