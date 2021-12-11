0156841459bd3ffa28c9bd2d7d8deaf70e711780
OleksandrVoitko <voytkomail@gmail.com>
2021-12-11 17:58:01 +0200

    add logout

--- src/js/components/firebase/firebaseAuth.js
+++ src/js/components/firebase/firebaseAuth.js
@@ -1,6 +1,13 @@
 import { firebaseConfig } from './firebaseConfig';
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -8,31 +15,92 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 const loginGoogleBtn = document.getElementById('login-google');
-loginGoogleBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+const logoutGoogleBtn = document.getElementById('logout-google');
 
-    signInWithPopup(auth, provider)
-      .then(result => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
+logoutGoogleBtn.style.display = 'none';
+checkAuthState();
 
-        console.log(`${user.displayName} - login with Google.`);
-      })
-      .catch(error => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
+// Login
+loginGoogleBtn.addEventListener('click', e => {
+  e.preventDefault();
 
-        console.log(errorCode + errorMessage);
-        console.log(email);
-        console.log(credential);
-      });
+  signInWithPopup(auth, provider)
+    .then(result => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+
+      loginGoogleBtn.style.display = 'none';
+      logoutGoogleBtn.style.display = 'block';
+      showUserName(user.displayName);
+      console.log(`${user.displayName} - login with Google.`);
+    })
+    .catch(error => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+
+      console.log(errorCode + errorMessage);
+      console.log(email);
+      console.log(credential);
+    });
+});
+
+// Logout
+logoutGoogleBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  signOut(auth)
+    .then(() => {
+      removeUserName();
+      loginGoogleBtn.style.display = 'block';
+      logoutGoogleBtn.style.display = 'none';
+      console.log(`Logout.`);
+    })
+    .catch(error => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+
+      console.log(errorCode + errorMessage);
+      console.log(email);
+      console.log(credential);
+    });
+});
+
+function showUserName(user) {
+  if (document.documentElement.clientWidth > 767) {
+    const name = document.createElement('p');
+    name.textContent = user;
+    name.id = 'user-name';
+    if (!document.getElementById('user-name')) {
+      document.getElementById('login').prepend(name);
+    }
+  }
+}
+
+function removeUserName() {
+  const userName = document.getElementById('user-name');
+  if (userName) userName.remove();
+}
+
+function checkAuthState() {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      loginGoogleBtn.style.display = 'none';
+      logoutGoogleBtn.style.display = 'block';
+      showUserName(user.displayName);
+    }
   });
+}
 
