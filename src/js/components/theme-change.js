8503dd6831aee571674f349f75dd1732ba244e50
Oleh Semennykov <sumcue@gmail.com>
2021-12-14 22:43:49 +0200

    newTheme

--- src/js/components/theme-change.js
+++ src/js/components/theme-change.js
@@ -4,61 +4,78 @@ const iconLight = document.querySelector('[data-theme="light"')
 themeBtn.addEventListener('click', onThemeBtn);
 
 
+if (!localStorage.theme) 
+    localStorage.theme = 'light'
+    document.body.className = localStorage.theme
 
- 
-if(!window.localStorage.getItem('Dark')){
-    window.localStorage.setItem('Light','light');
-    document.body.classList.toggle('light');
-    } else{
-        window.localStorage.setItem('Dark', 'dark');
-        document.body.classList.toggle('dark');
-     }
-     
-     if(!document.body.classList.contains('dark')) {
-    window.localStorage.setItem('Light','light');
-    document.body.classList.toggle('light');
- }else {
-    window.localStorage.setItem('Dark', 'dark');
-        document.body.classList.toggle('dark');
- }
- if(window.localStorage.getItem('')){
-    window.localStorage.setItem('Light', 'light');
-    document.body.classList.toggle('light')
+
+  
+
+
+
+
+function onThemeBtn () {
+document.body.classList.toggle("dark");
+
+localStorage.theme = document.body.className || "light";
+if(localStorage.theme === "dark") {
+
+}
+    
+
+   
+
+    
+
 }
 
 
+ 
+// if(!window.localStorage.getItem('Dark')){
+//     window.localStorage.setItem('Light', 'light');
+
+//     } else{
+//         window.localStorage.setItem('Dark','dark');
+       
+//      }
+     
+//      if(!document.body.classList.contains('dark')) {
+//     window.localStorage.setItem('Light','light');
+//     document.body.classList.toggle('light');
+//  }else {
+//     window.localStorage.setItem('Dark', 'dark');
+//         document.body.classList.toggle('dark');
+//  }
+
+
+
 
  
-function onThemeBtn(evt) {
+// function onThemeBtn(evt) {
     
-    if(!document.body.classList.contains('dark')) {
-        document.body.classList.remove('light');
-        document.body.classList.toggle('dark');
-        window.localStorage.setItem('Dark', 'dark');
-        
+//     if(!document.body.classList.contains('dark')) {
+//         document.body.classList.remove('light');
+//         document.body.classList.toggle('dark');
+//         }else {
+//         document.body.classList.remove('dark');
+//         document.body.classList.toggle('light');
+//         window.localStorage.setItem('Light', 'light'); 
      
-
-       
-    }else {
-        document.body.classList.remove('dark');
-        document.body.classList.toggle('light');
-        window.localStorage.setItem('Light', 'light'); 
-     
-    }
-    if(!window.localStorage.getItem('Dark')) {
-        window.localStorage.removeItem('Light');
-        window.localStorage.setItem('Dark', 'dark');
-    //    document.body.classList.toggle('dark')
+//     }
+//     if(!window.localStorage.getItem('Dark')) {
+//         window.localStorage.removeItem('Light');
+//         window.localStorage.setItem('Dark', 'dark');
+//     //    document.body.classList.toggle('dark')
       
-    }else {
-        window.localStorage.removeItem('Dark');
-        // document.body.classList.toggle('light');
-        window.localStorage.setItem('Light', 'light');
+//     }else {
+//         window.localStorage.removeItem('Dark');
+//         // document.body.classList.toggle('light');
+//         window.localStorage.setItem('Light', 'light');
        
-    }
-    //  if(window.localStorage.getItem('')){
-    //      window.localStorage.setItem('Dark', 'dark');
-    //  }
+//     }
+//     //  if(window.localStorage.getItem('')){
+//     //      window.localStorage.setItem('Dark', 'dark');
+//     //  }
   
- }
+//  }
 
