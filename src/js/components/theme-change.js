28dcc2784b90f2536cf5ebbe02dfaeaeb7260699
Oleh Semennykov <sumcue@gmail.com>
2021-12-14 02:57:04 +0200

    theme-change

--- src/js/components/theme-change.js
+++ src/js/components/theme-change.js
@@ -16,7 +16,7 @@ if(!window.localStorage.getItem('Dark')){
  }
  if(window.localStorage.getItem('')){
     window.localStorage.setItem('Light', 'light');
-    document.body.classList.toggle('Light')
+    document.body.classList.toggle('light')
 }
 
 
@@ -25,27 +25,22 @@ function onThemeBtn(evt) {
     console.log('aaaaaaa')
     if(!document.body.classList.contains('dark')) {
         document.body.classList.toggle('dark');
-        window.localStorage.setItem('Dark', 'dark');
+        // window.localStorage.setItem('Dark', 'dark');
         document.body.classList.remove('light');
-    } else{
+       
+    }else {
         document.body.classList.remove('dark');
         document.body.classList.toggle('light');
-        window.localStorage.setItem('Light', 'light'); 
+        // window.localStorage.setItem('Light', 'light'); 
     }
-    if(! window.localStorage.getItem('Dark')) {
-        window.localStorage.removeItem('Light');
-        window.localStorage.setItem('Dark', 'dark');
-    } else{
+    if(!window.localStorage.getItem('Dark')) {
+       window.localStorage.setItem('Dark', 'dark');
+       window.localStorage.removeItem('Light');
+    }else {
         window.localStorage.removeItem('Dark');
         window.localStorage.setItem('Light', 'light');
     }
      if(window.localStorage.getItem('')){
          window.localStorage.setItem('Dark', 'dark');
      }
-     if(window.localStorage.getItem('Light')){
-         window.localStorage.removeItem('Light');
-         window.localStorage.setItem('Dark', 'dark');
-         document.body.classList.toggle('dark');
-     }
-
-    }
+   }
